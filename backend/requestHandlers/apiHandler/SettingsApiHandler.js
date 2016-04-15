3cdf2117ca7dc314c50e3c8c54eeece175547c07
Owen Hardy <owen@iqmedia.nl>
2016-04-15 14:27:50 +0200

    settings changes now stored, still have to restart http server

--- backend/requestHandlers/apiHandler/SettingsApiHandler.js
+++ backend/requestHandlers/apiHandler/SettingsApiHandler.js
@@ -19,12 +19,32 @@ class SettingsApiHandler extends IApiHandler
         response.setHeader("Content-Type", "text/json");
         if(request.method=="PATCH")
         {
-            console.log(request);
+            var body = "";
+            request.on('data', function (data) {
+                body += data;
+            });
+            request.on('end', function () {
+                try{
+                    var data = JSON.parse(body);
+                    var attrs = data.data.attributes;
+                    for(var key in attrs)
+                    {
+                        Settings[key] = attrs[key];
+                    }
+                    Settings.save();
+                }catch(e){};
+                this.respond(response);
+            }.bind(this));
+        }else{
+            this.respond(response);
         }
+        return true;
+    }
 
+    respond(response)
+    {
         var json = JSON.stringify({data:{id:1, type:"setting", attributes:Settings}});
         response.end(json);
-        return true;
     }
 }
 
