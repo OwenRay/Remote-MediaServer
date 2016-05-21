a81f559a41744c18a2fd2746797a1920d5c77583
unknown <o_ray_hardy@hotmail.com>
2016-05-21 22:33:35 +0200

    now remembers where you left watching

--- frontend/app/components/html5-video.js
+++ frontend/app/components/html5-video.js
@@ -2,24 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     tagName:"video-container",
-    //attributeBindings: ['class'],
-    //class:"video-container",
 
     src:"",
     progress:0,
     videoObj:null,
     mediaItem:null,
     startOffset:0,
+    lastPosSave:0,
+
+    didInsertElement()
+    {
+        console.log("initvideo");
+        this.set("videoObj", this.$("video")[0]);
+        this.updateProgress();
+    },
 
     videoUrl:Ember.computed("mediaItem", "startOffset", function(){
         return "/ply/"+this.get("mediaItem.id")+"/"+this.get("startOffset");
     }),
 
-    didInsertElement()
+    didDestroyElement()
     {
-        this.set("videoObj", this.$("video")[0]);
-
-        this.updateProgress();
+        this.savePosition();
     },
 
     updateProgress()
@@ -30,7 +34,24 @@ export default Ember.Component.extend({
         if(this.videoObj.readyState === 4) {
             this.set("progress", this.videoObj.currentTime);
         }
+        if(new Date().getTime()-this.get("lastPosSave")>10000)
+        {
+            this.set("lastPosSave", new Date().getTime());
+            this.savePosition();
+        }
+
         Ember.run.later(this, "updateProgress", 500);
+    },
+
+    savePosition()
+    {
+        console.log("savepos");
+        var m = this.get("mediaItem");
+        m.get("play-position").then(function(lastPos){
+            lastPos.set("position", this.get("progress")+this.get("startOffset"));
+            lastPos.save();
+            console.log(lastPos);
+        }.bind(this));
     },
 
     actions:{
--- frontend/app/controllers/item/view.js
+++ frontend/app/controllers/item/view.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    confirmed:false,
+    startOffset:0,
+
+    showDialog:Ember.computed("model.play-position.position", "confirmed", function(){
+        if(!this.get("model.play-position.position"))
+        {
+            this.set("confirmed", true);
+        }
+        return !this.get("confirmed");
+    }),
+
+    actions:{
+        startFrom(time)
+        {
+            this.set("startOffset", time);
+            this.set("confirmed", true);
+        }
+    }
 });
