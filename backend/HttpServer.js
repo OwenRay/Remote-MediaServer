OwenRay/Remote-MediaServer

commit 77006bfca739acd403dcb3e3da6763edd20d2e0a

also moved the name parsing to a seperate class

backend/scanner/MovieScanner.js
@@ -1,11 +1,11 @@
 "use strict";
 
-var guessit = require("./Guessit");
 var recursive = require('recursive-readdir');
 var Settings = require("../Settings");
 var Database = require("../Database");
 var TheMovieDBExtendedInfo = require("./extendedInfo/TheMovieDBExtendedInfo");
 var FFProbeExtendedInfo = require("./extendedInfo/FFProbeExtendedInfo");
+var ParseFileNameExtendedInfo = require("./extendedInfo/ParseFileNameExtendedInfo");
 
 
 class MovieScanner
@@ -38,6 +38,7 @@ class MovieScanner
 
         this.types = Settings.getValue("videoFileTypes");
         this.library = Settings.getValue("libraries")[this.scanning];
+        console.log("start scan", this.library);
         recursive(this.library.folder, [this.willInclude.bind(this)], this.onListed.bind(this));
     }
 
@@ -59,73 +60,32 @@ class MovieScanner
     {
         if(err)
         {
-            // console.log(err);
+            console.log(err);
             return;
         }
         console.log("gotAllFiles");
-        var offset = -1;
-        var loadNext = function ()
+        for(var offset = 0; offset<files.length; offset++)
         {
-            offset++;
-            if(!files[offset]) {
-                this.checkForExtendedInfo();
-                return;
+            var file = files[offset].substr(this.library.folder.length);
+            if(!Database.findBy("media-item", "filepath", file).length) {
+                Database.setObject(
+                    "media-item",
+                    {
+                        filepath: file,
+                        libraryId: this.library.uuid,
+                        mediaType: this.library.type
+                    });
             }
-            var relativePath = files[offset].substr(this.library.folder.length);
-            if(Database.findBy("media-item", "filepath", relativePath).length!=0) {
-                return loadNext();
-            }
-
-            var filePath = path.parse(files[offset]);
-            var folder = path.parse(filePath.dir);
-
-            var errorFunction = function ()
-            {
-                 console.log("Fail");
-                guessit.parseName(folder.base.replace(/ /g, '.') + "-" + filePath.base.replace(/ /g, '.'), {options:"-t movie"}).then(function (data) {
-                    console.log(data);
-                    if(data.title) {
-                        data.title = data.title.replace(folder.base + '-', '');
-                    }
-                    this.applyGuessitData(data, relativePath);
-                    loadNext();
-                }.bind(this), function()
-                {
-                     console.log("andfail");
-                    loadNext();
-                });
-            }.bind(this);
-
-            guessit.parseName(filePath.base.replace(/ /g, '.'), {options:"-t movie"}).then(function (data) {
-                console.log(data);
-                if(this.applyGuessitData(data, relativePath)) {
-                    return loadNext();
-                }
-                errorFunction();
-            }.bind(this), errorFunction);
-        }.bind(this);
-        loadNext();
-    }
-
-    applyGuessitData(data, relativePath)
-    {
-        if(!data.title) {
-            //console.log("no title", data);
-            return false;
         }
-        data.libraryId = this.library.uuid;
-        data.mediaType = this.library.type;
-        data.filepath = relativePath;
-        Database.setObject("media-item", data);
-        return true;
+        this.checkForExtendedInfo();
     }
 
     checkForExtendedInfo()
     {
-        // console.log("checking for extended info...");
+        console.log("checking for extended info...");
         var items = Database.getAll("media-item");
 
-        var extendedInfoItems = [new TheMovieDBExtendedInfo(), new FFProbeExtendedInfo()];
+        var extendedInfoItems = [new ParseFileNameExtendedInfo(), new TheMovieDBExtendedInfo(), new FFProbeExtendedInfo()];
 
         var loadNext = function()
         {

backend/scanner/extendedInfo/FFProbeExtendedInfo.js
@@ -6,6 +6,7 @@
 var IExtendedInfo = require("./IExtendedInfo");
 var Promise = require("node-promise").Promise;
 var FFProbe = require('../../FFProbe');
+var Database = require("../../Database");
 
 class FFProbeExtendedInfo extends IExtendedInfo
 {

backend/scanner/extendedInfo/IExtendedInfo.js
@@ -8,11 +8,10 @@ class IExtendedInfo
 {
     /**
      *
-     * @param mediaItem
-     * @param done
+     * @param params
      * @return Promise
      */
-    extendInfo(mediaItem, library)
+    extendInfo(params)
     {
     }
 }

backend/scanner/extendedInfo/ParseFileNameExtendedInfo
@@ -1,0 +1,67 @@
+"use strict";
+/**
+ * Created by owenray on 29-06-16.
+ */
+var IExtendedInfo = require("./IExtendedInfo");
+var Guessit = require("../Guessit");
+var Promise = require("node-promise").Promise;
+var Database = require("../../Database");
+var path = require('path');
+
+class ParseFileNameExtendedInfo extends IExtendedInfo
+{
+    extendInfo(args, tryCount)
+    {
+        var mediaItem = args[0];
+        var library = args[1];
+        console.log(mediaItem);
+        if(!tryCount)
+        {
+            tryCount = 0;
+        }
+
+        var promise = new Promise();
+
+        var relativePath = mediaItem.attributes.filepath;
+
+        if(mediaItem.attributes.title) {
+            console.log("already parsed name!");
+            promise.resolve([mediaItem, library]);
+            return promise;
+        }
+
+        var filePath = path.parse(relativePath);
+        var folder = path.parse(filePath.dir);
+
+        var searchQuery = filePath.base.replace(/ /g, '.');
+
+        if(tryCount==1)
+        {
+            searchQuery = folder.base.replace(/ /g, '.') + "-" + filePath.base.replace(/ /g, '.');
+        }
+
+        Guessit.parseName(
+                searchQuery,
+                {options:"-t movie"}
+            ).then(
+                function (data) {
+                    console.log(data);
+                    if (tryCount == 1 && data.title) {
+                        data.title = data.title.replace(folder.base + '-', '');
+                    }
+                    if (data.title) {
+                        mediaItem.attributes.title = data.title;
+                        Database.update("media-item", mediaItem);
+                        return promise.resolve([mediaItem, library]);
+                    }
+                    this.extendInfo([mediaItem, library], tryCount + 1).then(resolve);
+                }.bind(this),
+                function() {
+                    resolve([mediaItem, library]);
+                }
+            );
+        return promise;
+    }
+}
+
+module.exports = ParseFileNameExtendedInfo;

backend/scanner/extendedInfo/TheMovieDBExtendedInfo.js
@@ -7,6 +7,7 @@ var IExtendedInfo = require("./IExtendedInfo");
 var Promise = require("node-promise").Promise;
 var MovieDB = require('moviedb')('0699a1db883cf76d71187d9b24c8dd8e');
 var path = require('path');
+var Database = require("../../Database");
 
 var discardRegex = new RegExp('\\W|-|_|([0-9]+p)|(LKRG)', "g");
 

backend/scanner/nameParsing/IParseName.js
@@ -1,13 +1,0 @@
-"use strict";
-/**
- * Created by owenray on 29-06-16.
- */
-
-class IParseName
-{
-    parse(mediaItem, done)
-    {
-    }
-}
-
-module.exports = IParseName;

commit da2074b4965b1d3ce7ce4603d9ce480638c2e659

index on scraping-fixes: 77006bf also moved the name parsing to a seperate class

backend/HttpServer.js
@@ -7,6 +7,7 @@ var FileRequestHandler = require('./requestHandlers/FileRequestHandler');
 var ApiRequestHandler = require('./requestHandlers/ApiRequestHandler');
 var PlayRequestHandler = require('./requestHandlers/PlayRequestHandler');
 var CorsRequestHandler = require('./requestHandlers/CorsRequestHandler');
+var ImageRequestHandler = require("./requestHandlers/ImageRequestHandler");
 var Settings = require('./Settings');
 var enableDestroy = require('server-destroy');
 
@@ -48,7 +49,8 @@ class HttpServer {
         var handlers = {
             api: ApiRequestHandler,
             ply: PlayRequestHandler,
-            web: FileRequestHandler
+            web: FileRequestHandler,
+			img: ImageRequestHandler
         };
         var part = request.url.substr(1, 3);
         if (!handlers[part]) {

backend/requestHandlers/ImageRequestHandler.js
@@ -1,0 +1,55 @@
+"use strict";
+/**
+ * Created by owenray on 08-04-16.
+ */
+var RequestHandler = require("./RequestHandler");
+var Database = require("../Database");
+var FFProbeImageHandler = require("./imageHandler/FFProbeImageHandler");
+var TmbdImageHandler = require("./imageHandler/TmbdImageHandler");
+var ImageCacheHandler = require("./imageHandler/ImageCacheHandler");
+var path = require("path");
+
+
+class CorsRequestHandler extends RequestHandler{
+    handleRequest()
+    {
+        var item = path.parse(this.request.url);
+        item = item.name.split("_");
+
+        var type = item[1];
+        item = Database.getById("media-item", item[0]);
+        this.response.setHeader("Content-Type", "image/jpeg");
+        if(!item)
+        {
+            return this.response.end();
+        }
+        var loop = [
+                //ImageCacheHandler,
+                TmbdImageHandler,
+                FFProbeImageHandler
+            ];
+        var promise = null;
+        for(var c = 0; c<loop.length&&!promise; c++)
+        {
+            promise = new loop[c]().getImageData(item, type);
+        }
+        if(promise)
+        {
+            promise.then(function(data)
+            {
+                if(c!=1) {
+                    new ImageCacheHandler().put(item, data, type);
+                }
+                console.log("return data");
+                this.response.end(data);
+            }.bind(this));
+        }else{
+            //redirect to no-img
+            console.log("could not get cover img");
+            this.response.end("");
+        }
+    }
+}
+
+//export RequestHandler;
+module.exports = CorsRequestHandler;

backend/requestHandlers/imageHandler/FFProbeImageHandler.js
@@ -1,0 +1,98 @@
+"use strict";
+/**
+ * Created by owenray on 7/9/16.
+ */
+var IImageHandler = require("./IImageHandler");
+var Promise = require("node-promise").Promise;
+var Database = require("../../Database");
+var spawn = require("child_process").spawn;
+var Settings = require("../../Settings");
+
+class FFProbeImageHandler extends IImageHandler
+{
+    getImageData(item, type)
+    {
+        var promise = new Promise();
+
+        var libraries = Settings.getValue("libraries");
+        var library;
+        var offset = 0;
+        if(!item.attributes.width)
+        {
+            return false;
+        }
+        if(item.attributes.fileduration)
+        {
+            offset = item.attributes.fileduration/2;
+        }
+
+        for(var key in libraries)
+        {
+            if(libraries[key].uuid==item.attributes.libraryId)
+            {
+                library = libraries[key];
+            }
+        }
+
+        var crop = {x:0, y:0,
+                    width:item.attributes.width, height:item.attributes.height};
+        var size = crop.width+"x"+crop.height;
+
+        if(type!="backdrop")
+        {
+            var targetSize = type=="poster"?{w:300,h:450}:{w:150,h:218};
+
+            size = targetSize.w+"x"+targetSize.h;
+            if(targetSize.w/targetSize.h>crop.width/crop.height)
+            {
+                crop.width = crop.width;
+                crop.height = crop.width*(targetSize.h/targetSize.w);
+            }else{
+                crop.height = crop.height;
+                crop.width = crop.height*(targetSize.w/targetSize.h);
+            }
+            crop.y+=Math.floor((item.attributes.height-crop.height)/2);
+            crop.x+=Math.floor((item.attributes.width-crop.width)/2);
+        }
+        crop = crop.width+":"+crop.height+":"+crop.x+":"+crop.y;
+
+        var file = library.folder+"/"+item.attributes.filepath;
+        var args = [
+            "-ss", offset,
+            "-i", file,
+            "-frames", "1",
+            "-filter", "crop="+crop,
+            "-y",
+            "-s", size,
+            "-f", "singlejpeg",
+            "-"
+        ];
+        console.log(args.join(" "));
+        var proc = spawn(
+            Settings.getValue("ffmpeg_binary"),
+            args);
+
+        var b;
+        proc.stdout.on("data", function(data){
+            if(b)
+            {
+                b = Buffer.concat([b, data]);
+            }else {
+                b = data;
+            }
+        }.bind(this));
+
+        proc.stderr.on('data', function(data)
+        {
+            console.log(`${data}`);
+        });
+
+        proc.on("close", function(){
+            promise.resolve(b);
+        }.bind(this));
+
+        return promise;
+    }
+}
+
+module.exports = FFProbeImageHandler;

backend/requestHandlers/imageHandler/IImageHandler.js
@@ -1,0 +1,17 @@
+"use strict";
+/**
+ * Created by owenray on 7/9/16.
+ */
+
+class IImageHandler
+{
+    getImageData(mediaItem, type)
+    {
+
+    }
+}
+
+IImageHandler.TYPE_BACKDROP = "backdrop";
+IImageHandler.TYPE_POSTER = "poster";
+
+module.exports = IImageHandler;

backend/requestHandlers/imageHandler/ImageCacheHandler.js
@@ -1,0 +1,35 @@
+"use strict";
+/**
+ * Created by owenray on 7/9/16.
+ */
+var IImageHandler = require("./IImageHandler");
+var Promise = require("node-promise").Promise;
+var fs = require("fs");
+
+class ImageCacheHandler extends IImageHandler
+{
+    getImageData(item, type)
+    {
+        var promise = new Promise();
+        var filename = "cache/thumb_"+item.id+"_"+type;
+        try {
+            console.log(fs.statSync(filename));
+        }catch(e)
+        {
+            console.log(e);
+            return false;
+        }
+        fs.readFile(filename, function(err, data){
+            promise.resolve(data);
+        });
+        return promise;
+    }
+
+    put(item, data, type)
+    {
+        var filename = "cache/thumb_"+item.id+"_"+type;
+        fs.writeFile(filename, data);
+    }
+}
+
+module.exports = ImageCacheHandler;

backend/requestHandlers/imageHandler/TmbdImageHandler.js
@@ -1,0 +1,41 @@
+"use strict";
+/**
+ * Created by owenray on 7/9/16.
+ */
+var IImageHandler = require("./IImageHandler");
+var Promise = require("node-promise").Promise;
+var http = require("http");
+
+class TmdbImageHandler extends IImageHandler
+{
+    getImageData(item, type)
+    {
+        console.log(item);
+        if(!item.attributes.gotExtendedInfo)
+        {
+            return false;
+        }
+        var w = "w150";
+        if(type==IImageHandler.TYPE_BACKDROP) {
+            w = "w1280";
+        }
+
+        var img = "http://image.tmdb.org/t/p/"+w+"/"+item.attributes[type+"-path"];
+        console.log(img);
+        var promise = new Promise();
+        http.get(img, function (response) {
+            var bytes = [];
+            response.on("data", function(data){
+                bytes.push(data);
+            });
+            response.on("end", function(){
+                promise.resolve(Buffer.concat(bytes));
+            });
+        });
+        
+
+        return promise;
+    }
+}
+
+module.exports = TmdbImageHandler;

backend/scanner/MovieScanner.js
@@ -85,7 +85,11 @@ class MovieScanner
         console.log("checking for extended info...");
         var items = Database.getAll("media-item");
 
-        var extendedInfoItems = [new ParseFileNameExtendedInfo(), new TheMovieDBExtendedInfo(), new FFProbeExtendedInfo()];
+        var extendedInfoItems = [
+                                    new FFProbeExtendedInfo(),
+                                    new ParseFileNameExtendedInfo(), 
+                                    new TheMovieDBExtendedInfo()
+                                ];
 
         var loadNext = function()
         {

backend/scanner/extendedInfo/FFProbeExtendedInfo.js
@@ -26,6 +26,9 @@ class FFProbeExtendedInfo extends IExtendedInfo
             {
                 if(!fileData||!fileData.format)
                     return promise.resolve(mediaItem, library);
+                
+                mediaItem.attributes.width = fileData.streams[0].width;
+                mediaItem.attributes.height = fileData.streams[0].height;
                 mediaItem.attributes.fileduration = parseFloat(fileData.format.duration);
                 mediaItem.attributes.filesize = parseInt(fileData.format.size);
                 mediaItem.attributes.bitrate = fileData.format.bit_rate;

frontend/app/models/media-item.js
@@ -14,14 +14,14 @@ export default DS.Model.extend({
 
     "backdrop-img-style":Ember.computed("backdrop-path", function()
     {
-        return "background-image:url(https://image.tmdb.org/t/p/w1280/"+this.get("backdrop-path")+")";
+        return "background-image:url(/img/"+this.get("id")+"_backdrop.jpg)";
     }),
     "poster-img":Ember.computed("poster-path", function()
     {
-        return "https://image.tmdb.org/t/p/w300/"+this.get("poster-path")+"";
+        return "/img/"+this.get("id")+"_poster.jpg";
     }),
     "poster-img-thumb-style":Ember.computed("poster-path", function()
     {
-        return "background-image:url(https://image.tmdb.org/t/p/w150/"+this.get("poster-path")+")";
+        return "background-image:url(/img/"+this.get("id")+"_postersmall.jpg)";
     })
 });

scripts/onrun.js
@@ -17,6 +17,11 @@ if(!fs.existsSync(dir))
 //make sure all settings files are in the right directory
 process.chdir(dir);
 
+if(!fs.existsSync("cache"))
+{
+    fs.mkdirSync("cache");
+}
+
 if(!fs.existsSync(dir+"ffmpeg")&&!fs.existsSync(dir+"ffmpeg.exe")) {
     console.log("downloading ffmpeg");
     http.get("http://downloadffmpeg.s3-website-eu-west-1.amazonaws.com/ffmpeg_" + os.platform() + "_" + os.arch() + ".zip", function (response) {