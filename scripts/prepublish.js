87ec37fdcda70da948b05eaff0101814eb94c0e5
Owen hardy <hardy.owen@gmail.com>
2017-12-09 17:21:37 +0100

    build fixes and setup.js fixes

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -5,5 +5,5 @@ var addToExec = require("os").platform()=="win32"?".cmd":"";
 process.chdir("frontend");
 
 console.log("Building react...");
-var res = spawnSync("npm"+addToExec, ["run", "build"]);
+var res = spawnSync("npm"+addToExec, ["run", "build", "--environment=production"]);
 console.log(`${res.stdout}`, `${res.stderr}`);
