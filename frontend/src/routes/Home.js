0884d2102fce568c39aa549dcf219a25f0cd7faa
Owen Hardy <OwenRay@users.noreply.github.com>
2018-12-07 19:35:35 +0100

    some more cleanup of animations implementation

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,33 +1,25 @@
 /**
  * Created by owenray on 6/30/2017.
  */
-import React, { Component } from 'react';
+import React from 'react';
 import { Card } from 'react-materialize';
 import { Flipped } from 'react-flip-toolkit';
 
-class home extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    if (!this.state) return null;
-    return (
-      <Flipped flipId="page">
-        <div>
-          <Card>
-            This will be the landingpage,<br />
-            enabling you to continue watching where you left of...<br />
-            some time in the future...
-          </Card>
-          <div className="homeLogo">
-            <img alt="Remote" src="/assets/img/logo.png" height={200} />
-          </div>
+function home() {
+  return (
+    <Flipped flipId="page">
+      <div>
+        <Card>
+          This will be the landingpage,<br />
+          enabling you to continue watching where you left of...<br />
+          some time in the future...
+        </Card>
+        <div className="homeLogo">
+          <img alt="Remote" src="/assets/img/logo.png" height={200} />
         </div>
-      </Flipped>
-    );
-  }
+      </div>
+    </Flipped>
+  );
 }
 
 export default home;
