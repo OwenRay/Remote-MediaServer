88704d1eec00f926fc488a551a46b3260b9cf85e
Owen Hardy <OwenRay@users.noreply.github.com>
2019-01-23 18:52:32 +0100

    WIP, range request handler

--- backend/requestHandlers/ply/Mpeg4PlayHandler.js
+++ backend/requestHandlers/ply/Mpeg4PlayHandler.js
@@ -11,16 +11,39 @@ const Log = require('../../helpers/Log');
 const RequestHandler = require('../RequestHandler');
 const { PassThrough } = require('stream');
 
+const sessions = {};
+const backBufferSize = 15 * 1000 * 1000;
+
 class Mpeg4PlayHandler extends RequestHandler {
   handleRequest() {
-    const mediaItem = Database.getById('media-item', this.context.params.id);
+    if (!this.context.query.id) {
+      this.response.status = 302;
+      this.response.set('Location', `?id=${Math.random()}`);
+      return;
+    }
     this.context.set('Accept-Ranges', 'none');
+    this.context.set('Content-Type', 'video/webm');
+
+    const promise = new Promise((resolve) => {
+      this.resolve = resolve;
+    });
+    //this.context.set('Content-Range', 'bytes 0-');
+    this.buffer = Buffer.alloc(backBufferSize);
+    this.offset = 0;
+
+    if (sessions[this.context.query.id]) {
+      sessions[this.context.query.id].attach(this.context);
+      return;
+    }
+
+    sessions[this.context.query.id] = this;
+    const mediaItem = Database.getById('media-item', this.context.params.id);
     this.ffmpeg = new FFMpeg(mediaItem, '-')
       .setPlayOffset(this.context.params.offset)
       .setOnReadyListener(this.onFFMpegReady.bind(this))
       .addOutputArguments([
         '-f', 'mp4',
-        '-movflags', 'empty_moov',
+        '-movflags', 'frag_keyframe+empty_moov',
       ]);
     if (this.context.query.audioChannel) {
       this.ffmpeg.setAudioChannel(this.context.query.audioChannel);
@@ -28,9 +51,7 @@ class Mpeg4PlayHandler extends RequestHandler {
     if (this.context.query.videoChannel) {
       this.ffmpeg.setVideoChannel(this.context.query.videoChannel);
     }
-    const promise = new Promise((resolve) => {
-      this.resolve = resolve;
-    });
+
 
     this.ffmpeg.run();
     this.bufferedChuncks = 0;
@@ -40,29 +61,81 @@ class Mpeg4PlayHandler extends RequestHandler {
   }
 
   onFFMpegReady() {
-    this.context.req.connection.on('close', () => {
-      Log.debug('close video play connection!');
-      this.ffmpeg.kill();
-    });
 
-    this.ffmpeg.getOutputStream().on('data', this.onData.bind(this));
+    if(!this.offset) {
+      this.ffmpeg.getOutputStream().on('data', this.onData.bind(this));
+    }
 
     this.context.body = new PassThrough();
+
     this.context.body.on('close', () => {
-      this.ffmpeg.kill();
+      Log.debug('close video play connection!');
+      this.ffmpeg.pause();
+      // wait 20 minutes before killing the session
+      // (seems like enough time for the average break to me;)
+      this.waitToKill = setTimeout(this.kill.bind(this), 1200000);
     });
     this.resolve();
   }
 
+  kill() {
+    this.ffmpeg.kill();
+  }
+
+  attach(context) {
+    this.context = context;
+    this.onFFMpegReady();
+    const range = this.context.get('Range');
+    let offset = 0;
+    if (range) {
+      offset = parseInt(range.split('=')[1].split('-')[0], 10);
+    }
+    if (offset > this.offset - backBufferSize && offset < this.offset) {
+      const size = this.offset - offset;
+      if (size < backBufferSize) {
+        const localOffset = (this.offset - size) % backBufferSize;
+        const buf = Buffer.alloc(size);
+        const i = this.buffer.copy(buf, 0, localOffset);
+        this.buffer.copy(buf, i, size - i);
+
+        this.context.set('Content-Range', `bytes ${offset}-`);
+        clearTimeout(this.waitToKill);
+        this.waitToKill = false;
+        const written = this.context.body.write(buf, () => {
+          this.ffmpeg.resume();
+        });
+        console.log(written);
+      }
+    } else {
+      this.buffer = [];
+      console.log("need implement reencode");
+    }
+  }
+
   onData(data) {
+    const offset = this.offset % backBufferSize;
+    if (offset + data.length >= backBufferSize) {
+      data.copy(this.buffer, offset, 0, backBufferSize - offset);
+      data.copy(this.buffer, 0, (offset + data.length) % backBufferSize);
+    } else {
+      data.copy(this.buffer, this.offset);
+    }
+    this.offset += data.length;
+
+    if (this.waitToKill) {
+      console.log('ondata after close!!!');
+    }
+
     this.bufferedChuncks += 1;
     let pause = !this.context.body.write(data, () => {
       this.bufferedChuncks -= 1;
-      if (this.ffmpeg.getOutputStream().isPaused() && this.bufferedChuncks <= 19) {
+      if (!this.waitToKill &&
+        this.ffmpeg.getOutputStream().isPaused() &&
+        this.bufferedChuncks <= 19) {
         this.ffmpeg.getOutputStream().resume();
       }
     });
-    pause = pause || this.bufferedChuncks > 20;
+    pause = pause || this.bufferedChuncks >= 20;
     if (pause) {
       this.ffmpeg.getOutputStream().pause();
     }
